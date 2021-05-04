97a2c026f72716605dcaf3c69d7552bffdad0097
samira <sboudiaf@arfp.asso.fr>
2021-05-04 12:33:51 +0200

    exo 3 finit

---
+++ anneeBisextileOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace anneeBisextileOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez une année");
+            int annee = Int32.Parse(Console.ReadLine());
+
+            if (isBisextile(annee))
+                Console.WriteLine("L'année est bisextile");
+            else
+                Console.WriteLine("L'année n'est pas bisextile");
+
+        }
+
+        public static bool isBisextile(int year)
+        {
+           
+            if (DateTime.IsLeapYear(year))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ exoValeurAbsolue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exoValeurAbsolue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
